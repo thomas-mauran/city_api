8d58246b96dce0feee2dc3e6631c7089ff8a5cd7
Mauran <thomas.mauran@etu.umontpellier.fr>
2023-05-24 15:38:48 +0200

    feat(utils): response formater

--- src/main.go
+++ src/main.go
@@ -5,13 +5,15 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/thomas-mauran/city_api/src/utils"
 )
 
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+	r.Get("/_health", func(w http.ResponseWriter, r *http.Request) {
+		utils.Response(w, r, 204, "welcome")
+		return
 	})
 	http.ListenAndServe(":3000", r)
 }
---
+++ src/utils/http.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"net/http"
+
+	"github.com/go-chi/render"
+)
+
+func Response(w http.ResponseWriter, r *http.Request, code int, msg string) {
+	render.Status(r, code)
+	render.JSON(w, r, map[string]string{"message": msg})
+}
